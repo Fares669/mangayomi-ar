f0c8cd93174d176a2b0511ab7a36139d1efcdcf3
9vsv6 <113776801+9vsv6@users.noreply.github.com>
2026-06-21 14:45:02 +0400

    add manga spark

---
+++ manga/mangaspark.dart
@@ -0,0 +1,14 @@
+import '../../../../../../../model/source.dart';
+
+Source get mangasparkSource => _mangasparkSource;
+Source _mangasparkSource = Source(
+  name: "Manga Spark",
+  baseUrl: "https://manga-spark.net",
+  lang: "ar",
+  isNsfw: false,
+  typeSource: "madara",
+  iconUrl:
+      "https://www.google.com/s2/favicons?sz=256&domain=https://manga-spark.net",
+  dateFormat: "",
+  dateFormatLocale: "",
+);
